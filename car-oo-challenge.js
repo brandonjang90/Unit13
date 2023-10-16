e7e00337aea4893f3a326dbba048fe88abcd4e35
Brandon Jang <brandonjang90@gmail.com>
2023-10-15 21:08:05 -0700

    finished exercise

---
+++ car-oo-challenge.js
@@ -0,0 +1,56 @@
+//PART 1
+class Vehicle {
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+ 
+    }
+    honk(){
+        return 'Beep';
+    }
+    toString(){
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+}
+
+
+//PART 2
+class Car extends Vehicle {
+    constructor(make, model, year){
+    super(make, model, year);
+    this.numWheels = 4;
+    }
+
+}   
+
+//PART 3
+class Motorcycle extends Vehicle{
+    constructor(make, model, year){
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+
+    revEngine(){
+       return 'VROOM!!!'; 
+    }
+}
+
+//PART 4
+class Garage {
+    constructor(capacity){
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(newVehicle){
+        if (!(newVehicle instanceof Vehicle)){
+            return "Only vehicles are allowed in here"
+        }
+        if (this.vehicles.length >= this.capacity){
+            return "Sorry, we're full."
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!"
+    }
+}
